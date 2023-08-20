d27f355835e8aa6e8b16e5aae80c3c1b546f332c
Deadpool1 <atwood169@yahoo.com>
2023-08-20 10:51:26 -0500

    initialized Contact

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+const Contact = () => {
+    return (
+        <div className="contact">
+            <h2>Contact Us</h2>
+            <p>Phone: 612-782-6930</p>
+            <p>Fax: 612-782=</p>
+            <p>Email: info@customproductsandservices.com</p>
+
+            <p>Address:</p>
+            <p></p>
+        </div>
+    )
+}
+
+export default Contact
