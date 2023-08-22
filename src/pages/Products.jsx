151a79026213be0cebfa5c9b574be188ee38331d
Deadpool1 <atwood169@yahoo.com>
2023-08-22 14:17:26 -0500

    initialized Products page

---
+++ src/pages/Products.jsx
@@ -0,0 +1,11 @@
+import steinerlogo from '../assets/img/Steiner-Logo.jpeg'
+
+const Products = () => {
+    return (
+        <div className="products">
+            <img src={steinerlogo} alt='Steiner Logo'/>
+        </div>
+    )
+}
+
+export default Products
