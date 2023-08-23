ea1aa8521b9b4e061acdbb53572c1c7d764e718f
Deadpool1 <atwood169@yahoo.com>
2023-08-23 14:10:41 -0500

    added list items to Services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,7 +2,26 @@ const Services = () => {
     return (
         <div className="services">
             <h1>Services</h1>
-            <p>We Offer a complete range of property services including:</p>
+            <h2>We Offer a complete range of property services including:</h2>
+            <ul>
+                <li>Snow Removal and Hauling</li>
+                <li>Sidewalk Snow Removal With Specialized Equipmen</li>
+                <li>De-icing</li>
+                <li>Weekly Mowing</li>
+                <li>Weed and Pest Control</li>
+                <li>Landscape Installation</li>
+                <li>Pruning</li>
+                <li>Aeration</li>
+                <li>Sod and Seed</li>
+                <li>Holiday Lighting and Decorations</li>
+                <li>Graffiti Abatement</li>
+                <li>Tree Planting</li>
+                <li>Annual and Perennial Planting</li>
+                <li>Lot Sweeping</li>
+                <li>Organic Gardens</li>
+                <li>Garden Planting and Maintenance</li>
+
+            </ul>
         </div>  
     )
 }
