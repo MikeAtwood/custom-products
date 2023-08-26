0ebd507afecde02d880b3c746a1d94459069a431
Deadpool1 <atwood169@yahoo.com>
2023-08-26 13:20:33 -0500

    styled about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,23 @@
 const About = () => {
     return (
         <div className="about">
-            <h2>About Us</h2>
-            <h1>    
+            <h1>About Us</h1>
+            <h2>    
                 Our company is based on the belief that our customers needs are of the utmost importance. Our entire team 
                 is committed to meeting those needs. As a result, a high percentage of our business is from repeat customers and 
                 referrals. We would welcome the opportunity to earn your trust and deliver you the best service in the industry.
-            </h1>
-            <h2>
+            </h2>
+            <h3>
                 Location:
                 2919 Central Avenue NE
                 Minneapolis, MN 55418
-            </h2>
-            <h2>
+            </h3>
+            <h3>
                 Hours:
                 Mon - Fri: 9AM - 5PM
                 Sat: Closed         
                 Sun: Closed
-            </h2>
+            </h3>
         </div>
         
     )
