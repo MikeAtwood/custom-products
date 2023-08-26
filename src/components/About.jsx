382e018764625827dffdd131ecdb6e1e6fc52429
Deadpool1 <atwood169@yahoo.com>
2023-08-26 10:10:04 -0500

    about us styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,23 +2,22 @@ const About = () => {
     return (
         <div className="about">
             <h2>About Us</h2>
-            <h1>Our company is based on the belief that our customers needs are of the utmost importance. Our entire team is committed to meeting those needs. As a result, a high percentage of our business is from repeat customers and referrals.
-
-We would welcome the opportunity to earn your trust and deliver you the best service in the industry.
-
-Location:
-
-2919 Central Avenue NE
-
-Minneapolis, MN 55418
-
-Hours:
-
-Mon - Fri: 9AM - 5PM
-
-Sat: Closed
-
-Sun: Closed</h1>
+            <h1>    
+                Our company is based on the belief that our customers needs are of the utmost importance. Our entire team 
+                is committed to meeting those needs. As a result, a high percentage of our business is from repeat customers and 
+                referrals. We would welcome the opportunity to earn your trust and deliver you the best service in the industry.
+            </h1>
+            <h2>
+                Location:
+                2919 Central Avenue NE
+                Minneapolis, MN 55418
+            </h2>
+            <h2>
+                Hours:
+                Mon - Fri: 9AM - 5PM
+                Sat: Closed         
+                Sun: Closed
+            </h2>
         </div>
         
     )
