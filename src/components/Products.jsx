2e3b6abc06cc6b7e97b0cb41b737d5058ffea7df
Deadpool1 <atwood169@yahoo.com>
2023-08-27 08:03:03 -0500

    styled products text

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,24 +4,24 @@ import '../App.css'
 const Products = () => {
     return (
         <div className="products">
-            
+            <div className='text-background'>
             <img src={steinerlogo} alt='Steiner Logo'/>
-            <p>
-                When there is work to be done, you’ll get more of it done with a Steiner tractor! No matter what the job, 
-                there’s a Steiner tractor up to the task. Whether the job calls for mowing a lawn or maintaining a ball field,
-                moving dirt or hay, plowing snow, or sweeping golf cart paths, Steiner tractors can tackle it all. The Steiner 430
-                4-wheel drive tractor combines articulating steering, an oscillating frame and low center of gravity to conquer the 
-                toughest conditions. The NEW Steiner 235 tractor is more than the average tractor or front-mount mower. With 
-                plenty of power and versatility, the 235 front-wheel drive tractor delivers a wallop of a hard-working punch. 
-                A low center of gravity and rear-wheel power steering makes it more stable, maneuverable, and comfortable to
-                 operate. You can equip all Steiner tractors with a wide range of turf, grounds maintenance, and snow 
-                 removal attachments that mount in minutes without tools thanks to our exclusive Steiner Quick-Hitch™ System. 
-                 A Steiner tractor is the best investment you can make in powered equipment!
-            </p>
-            <p>Visit www.steinerturf.com for complete specifications.</p>
-
-            <p>Call us @ 612-782-6930 for pricing or schedule a demonstration.</p>
-            
+                <p>
+                    When there is work to be done, you’ll get more of it done with a Steiner tractor! No matter what the job, 
+                    there’s a Steiner tractor up to the task. Whether the job calls for mowing a lawn or maintaining a ball field,
+                    moving dirt or hay, plowing snow, or sweeping golf cart paths, Steiner tractors can tackle it all. The Steiner 430
+                    4-wheel drive tractor combines articulating steering, an oscillating frame and low center of gravity to conquer the 
+                    toughest conditions. The NEW Steiner 235 tractor is more than the average tractor or front-mount mower. With 
+                    plenty of power and versatility, the 235 front-wheel drive tractor delivers a wallop of a hard-working punch. 
+                    A low center of gravity and rear-wheel power steering makes it more stable, maneuverable, and comfortable to
+                     operate. You can equip all Steiner tractors with a wide range of turf, grounds maintenance, and snow 
+                     removal attachments that mount in minutes without tools thanks to our exclusive Steiner Quick-Hitch™ System. 
+                     A Steiner tractor is the best investment you can make in powered equipment!
+                </p>
+                <p>Visit www.steinerturf.com for complete specifications.</p>
+        
+                <p>Call us @ 612-782-6930 for pricing or schedule a demonstration.</p>
+            </div>
         </div>
     )
 }
