22dbcf193ac114854d17129f5089ac3eb8ef1a3b
Deadpool1 <atwood169@yahoo.com>
2023-09-17 09:58:27 -0500

    updated about id

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
     return (
-        <div className="about">
+        <div className="about" id="about">
             <h1>About Us</h1>
             <h2>    
                 Our company is based on the belief that our customers needs are of the utmost importance. Our entire team 
