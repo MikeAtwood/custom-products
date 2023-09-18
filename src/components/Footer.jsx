14b3d47797ea28eeeb21be18ab81859f26955173
Deadpool1 <atwood169@yahoo.com>
2023-09-18 16:08:40 -0500

    added copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,17 @@ import Logo from '../assets/img/logo.png'
 const Footer = () => {
     return (
         <div className="footer-container">
-            <img src={Logo} />
-            <h1>Custom Products & Services</h1>
-            <hr className='footer-line' />
+           
+                <img src={Logo} />
+                <h1>Custom Products & Services</h1>
+                <hr className='footer-line' />
+           
+            
+            
+            <div className='footer-underline'>
+                <p>©Custom Products & Services. All Rights Reserved 2023</p>
+            </div>
+            
         </div>
     )
 }
