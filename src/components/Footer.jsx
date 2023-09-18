f3558c56d6530942638f2741080b038f3c85a545
Deadpool1 <atwood169@yahoo.com>
2023-09-18 15:37:30 -0500

    added png to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
+import Logo from '../assets/img/logo.png'
+
 const Footer = () => {
     return (
         <div className="footer-container">
+            <img src={Logo} />
             <h1>Custom Products & Services</h1>
         </div>
     )
