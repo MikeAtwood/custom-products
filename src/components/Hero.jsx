adbc17704a993d343766284b5f4c8a72ba2f8aa3
Deadpool1 <atwood169@yahoo.com>
2023-09-19 16:34:06 -0500

    working on media query

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,12 @@
 import '../App.css'
+import heroImg from '../assets/img/hero-img.jpeg'
 
 
 const Hero = () => {
     return (
         <section className='hero-container' id="home">
+            
+            <img src={heroImg} />
             <h1>Custom Products & Services</h1>
         </section>
     )
