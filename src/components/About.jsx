164de799aef958ebee4b7bd10dfd6d0360dffb0b
Deadpool1 <atwood169@yahoo.com>
2023-09-25 12:36:41 -0500

    fixed hero display

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,31 @@
+// import treeImg from '../assets/img/hand-tree.jpg'
+
 const About = () => {
     return (
         <div className="about-wrapper">
             <div className="about" id="about">
                 <h1>About Us</h1>
-                <h2>    
-                    Our company is based on the belief that our customers needs are of the utmost importance. Our entire team 
-                    is committed to meeting those needs. As a result, a high percentage of our business is from repeat customers and 
-                    referrals. We would welcome the opportunity to earn your trust and deliver you the best service in the industry.
-                </h2>
-                <h3>
-                    Location:
-                    2919 Central Avenue NE
-                    Minneapolis, MN 55418
-                </h3>
-                <h3>
-                    Hours:
-                    Mon - Fri: 9AM - 5PM
-                    Sat: Closed         
-                    Sun: Closed
-                </h3>
+                <div className="left-column">
+                    <h2>    
+                        Our company is based on the belief that our customers needs are of the utmost importance. Our entire team 
+                        is committed to meeting those needs. As a result, a high percentage of our business is from repeat customers and 
+                        referrals. We would welcome the opportunity to earn your trust and deliver you the best service in the industry.
+                    </h2>
+                    <h3>
+                        Location:
+                        2919 Central Avenue NE
+                        Minneapolis, MN 55418
+                    </h3>
+                    <h3>
+                        Hours:
+                        Mon - Fri: 9AM - 5PM
+                        Sat: Closed         
+                        Sun: Closed
+                    </h3>
+                </div>
+                <div className="right-column">
+                    {/* <img src={treeImg} /> */}
+                </div>
             </div>
         </div>
     )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,16 @@
 import '../App.css'
 // import heroImg from '../assets/img/hero-img.jpeg'
 // import heroImg from '../assets/img/hero-mn.jpeg'
-import heroImg from  '../assets/img/hero2.jpeg'
+// import heroImg from  '../assets/img/hero2.jpeg'
 
 
 const Hero = () => {
     return (
         <section className='hero-container' id="home">
             
-            <img src={heroImg} />
-            <h1 className='animate__animated animate__backInRight'>Custom Products & Services</h1>
-            <p>Serving the greater Minneapolis area since 19XX</p>
+            {/* <img src={heroImg} /> */}
+            <h1 className='animate__animated animate__backInDown'>Custom Products & Services</h1>
+            <p className='animate__animated animate__backInDown'>Serving the greater Minneapolis area since 19XX</p>
         </section>
     )
 }
