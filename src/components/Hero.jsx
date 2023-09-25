0e4c088671b8acba84a6af37b4e6bb0c9ffa77a0
Deadpool1 <atwood169@yahoo.com>
2023-09-25 10:38:36 -0500

    updated desktop hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,7 @@
 import '../App.css'
-import heroImg from '../assets/img/hero-img.jpeg'
+// import heroImg from '../assets/img/hero-img.jpeg'
+// import heroImg from '../assets/img/hero-mn.jpeg'
+import heroImg from  '../assets/img/hero2.jpeg'
 
 
 const Hero = () => {
@@ -8,6 +10,7 @@ const Hero = () => {
             
             <img src={heroImg} />
             <h1 className='animate__animated animate__backInRight'>Custom Products & Services</h1>
+            <p>Serving the greater Minneapolis area since 19XX</p>
         </section>
     )
 }
