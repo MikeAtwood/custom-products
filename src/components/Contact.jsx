b82478e9cb5984b882e2ba776a315ed3e110b6c6
Deadpool1 <atwood169@yahoo.com>
2023-09-29 10:39:48 -0500

    styled contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
                 <p>Fax: 612-782-6923</p>
                 <a href="mailto:info@customproductsandservices.com">Email: info@customproductsandservices.com</a>
 
-                <p>Address:</p>
+                <p className='address-container'>Address:</p>
                 <p>Custom Products & Services
                     2919 Central Avenue North East
                     Minneapolis, MN 55418</p>
