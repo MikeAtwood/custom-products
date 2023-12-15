bf91a07d8502ddec8d5ffcc94e987d39a9fc94bf
Deadpool1 <atwood169@yahoo.com>
2023-12-15 16:20:07 -0600

    refactored footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,11 @@ import Logo from '../assets/img/logo.png'
 const Footer = () => {
     return (
         <div className="footer-container">
-           
-                <img src={Logo} />
-                <hr className='footer-line' />
-           
-            
-            
+            <img src={Logo} alt='Logo'/>
+            <hr className='footer-line' />
             <div className='footer-underline'>
                 <p>©Custom Products & Services. All Rights Reserved 2023</p>
             </div>
-            
         </div>
     )
 }
