858a666dd4196ada6ae8d8bc1cc4e10563dbf093
Deadpool1 <atwood169@yahoo.com>
2024-01-17 16:09:23 -0600

    refactored hero code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,18 @@
 import '../App.css'
 // import heroImg from '../assets/img/hero-img.jpeg'
 // import heroImg from '../assets/img/hero-mn.jpeg'
-// import heroImg from  '../assets/img/hero2.jpeg'
+import heroImg from  '../assets/img/hero2.jpeg'
 
 
 const Hero = () => {
     return (
         <section className='hero-container' id="home">
-            
-            {/* <img src={heroImg} /> */}
-            <h1 className='animate__animated animate__backInDown'>Custom Products & Services</h1>
-            <p className='animate__animated animate__backInDown'>Serving the greater Minneapolis area since 19XX</p>
-            <a href="mailto:info@customproductsandservices.com" className='email-button'>
-                Email Us!
-            </a>
+            <img className='hero-img' src={heroImg} />
+                <div className='hero-info'>
+                    <h1 className='animate__animated animate__backInDown'>Custom Products & Services</h1>
+                    <p className='animate__animated animate__backInDown'>Serving the greater Minneapolis area since 19XX</p>
+                    <a href="mailto:info@customproductsandservices.com" className='email-button'>Email Us!</a>
+                </div>
         </section>
     )
 }
