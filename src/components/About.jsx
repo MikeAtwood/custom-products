49dbe9abf2ef920954c2b6320deacc540f8cb9da
Deadpool1 <atwood169@yahoo.com>
2024-01-17 14:00:26 -0600

    updated about container

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             <div className="about" id="about">
                 <div className="about-container">
                     <h1>About Us</h1>
-                    <h2>    
+                    <h2 className='about-info'>    
                         Our company is based on the belief that our customers needs are of the utmost importance. Our entire team 
                         is committed to meeting those needs. As a result, a high percentage of our business is from repeat customers and 
                         referrals. We would welcome the opportunity to earn your trust and deliver you the best service in the industry.
