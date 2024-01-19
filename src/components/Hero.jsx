299cb375c98ddd3115ebf6087db80d8318dfba7d
Deadpool1 <atwood169@yahoo.com>
2024-01-19 00:03:55 -0600

    created sticky div for hero img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,9 @@ import heroImg from  '../assets/img/hero2.jpeg'
 const Hero = () => {
     return (
         <section className='hero-container' id="home">
-            <img className='hero-img' src={heroImg} />
+            <div className='sticky-hero'>
+                <img className='hero-img' src={heroImg} />
+            </div>
                 <div className='hero-info'>
                     <h1 className='animate__animated animate__backInDown'>Custom Products & Services</h1>
                     <p className='animate__animated animate__backInDown'>Serving the greater Minneapolis area since 19XX</p>
