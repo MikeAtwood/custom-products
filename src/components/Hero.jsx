5bec1e1aef2a12ffc6aed122b0d6c2a9e4404333
Deadpool1 <atwood169@yahoo.com>
2024-01-19 00:11:15 -0600

    working on sticky hero img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,7 @@ import heroImg from  '../assets/img/hero2.jpeg'
 const Hero = () => {
     return (
         <section className='hero-container' id="home">
-            <div className='sticky-hero'>
-                <img className='hero-img' src={heroImg} />
-            </div>
+            <img className='hero-img' src={heroImg} />
                 <div className='hero-info'>
                     <h1 className='animate__animated animate__backInDown'>Custom Products & Services</h1>
                     <p className='animate__animated animate__backInDown'>Serving the greater Minneapolis area since 19XX</p>
@@ -20,3 +18,14 @@ const Hero = () => {
 }
 
 export default Hero
+
+{/* <div>
+<section className='hero-container' id="home">
+    <img className='hero-img' src={heroImg} />
+</section>    
+<div className='hero-info'>
+    <h1 className='animate__animated animate__backInDown'>Custom Products & Services</h1>
+    <p className='animate__animated animate__backInDown'>Serving the greater Minneapolis area since 19XX</p>
+    <a href="mailto:info@customproductsandservices.com" className='email-button'>Email Us!</a>
+</div>
+</div> */}
