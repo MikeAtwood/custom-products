8fd53cecca373b7512a5db11509090860a1fa809
Deadpool1 <atwood169@yahoo.com>
2024-01-27 11:29:29 -0600

    reverted gallery code

---
+++ src/components/Albums.jsx
@@ -0,0 +1,12 @@
+import '../../src/Gallery.css';
+
+const Albums = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Albums;
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -19,3 +19,30 @@ const Gallery = () => {
 }
 
 export default Gallery
+
+// import { HashRouter as Router, Route, Link, Switch  } from 'react-router-dom'
+// import '../Gallery.css'
+// import Footer from '../components/Footer'
+// import Collection from '../components/collection'
+// import Albums from '../components/Albums'
+
+// const Gallery = () => {
+//     return (
+//         <Router>
+//             <div className="gallery-main">
+//                 <h1>Gallery</h1>
+//                     <nav className='GalleryButtons'>
+//                         <Link to="/photos">Photos</Link>
+//                         <Link to="/albums">Albums</Link>
+//                     </nav>
+//             </div>
+//             <Switch>
+//                 <Route path='/photos' component={Collection} />
+//                 <Route path='/albums' component={Albums} />
+//             </Switch>
+//             <Footer />
+//         </Router>
+//     )
+// }
+
+// export default Gallery
