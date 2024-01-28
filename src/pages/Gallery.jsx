9ecec476f66caf60536309bac9759b5b8b836bcd
Deadpool1 <atwood169@yahoo.com>
2024-01-28 13:08:31 -0600

    added useState in Gallery

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,18 +1,25 @@
+import { useState } from 'react'
 import '../Gallery.css'
 import Footer from '../components/Footer'
 import Collection from '../components/collection'
+import Albums from '../components/Albums'
 
 const Gallery = () => {
+    const [ showPhotos, setShowPhotos ] = useState(true);
+
+    const handleButtonClick = () => {
+        setShowPhotos(!showPhotos)
+    }
     return (
         <>
             <div className="gallery-main">
                 <h1>Gallery</h1>
                     <div className='GalleryButtons'>
-                        <button>Photos</button>
-                        <button>Albums</button>
+                        <button onClick={handleButtonClick}>Photos</button>
+                        <button onClick={handleButtonClick}>Albums</button>
                     </div>
-            </div>
-            <Collection />
+                </div>
+            {showPhotos ? <Collection /> : <Albums />}
             <Footer />
         </>
     )
