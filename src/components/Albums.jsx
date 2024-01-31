beadd9c911001ff0eb6327d51c9b12af8da2b272
Deadpool1 <atwood169@yahoo.com>
2024-01-31 11:10:26 -0600

    added albums photo div

--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -1,12 +1,13 @@
 import '../../src/Gallery.css';
 import { useState } from 'react';
+
 const Albums = () => {
     const [selectedAlbum, setSelectedAlbum] = useState(null)
 
     const albums = [
         {id: 1, name: "Album 1", photos: ['dummy1.jpeg', 'dummy2.jpeg', 'dummy3.jpeg'] },
-        {id: 1, name: "Album 1", photos: ['dummy4.jpeg', 'dummy5.jpeg', 'dummy6.jpeg'] },
-        {id: 1, name: "Album 1", photos: ['dummy7.jpeg', 'dummy8.jpeg', 'dummy9.jpeg', 'dummy10.jpeg', 'dummy11.jpeg'] },
+        {id: 1, name: "Album 2", photos: ['dummy4.jpeg', 'dummy5.jpeg', 'dummy6.jpeg'] },
+        {id: 1, name: "Album 3", photos: ['dummy7.jpeg', 'dummy8.jpeg', 'dummy9.jpeg', 'dummy10.jpeg', 'dummy11.jpeg'] },
     ]
 
     const handleAlbumClick = (album) => {
@@ -27,6 +28,14 @@ const Albums = () => {
                     </div>
                 ))}
             </div>
+            {selectedAlbum && (
+                <div className='photo-list'>
+                    <h3>{selectedAlbum.name}</h3>
+                    {selectedAlbum.photos.map((photo) => (
+                        <img key={photo} src={photo} alt={photo} />
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
