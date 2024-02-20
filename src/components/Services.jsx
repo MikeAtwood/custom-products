32d48e76b648c87eeef4599466ecf5cc79f57c2a
Deadpool1 <atwood169@yahoo.com>
2024-02-20 10:43:56 -0600

    commented out carousel import

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,4 @@
-import { Carousel } from "react-responsive-carousel";
+// import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 // import Summer from "../assets/img/grass.jpeg";
 // import Fall from "../assets/img/leaves.jpeg";
